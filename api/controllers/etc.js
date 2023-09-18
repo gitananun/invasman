4b130b1f4ab2e7061b5f5a8ea1364c92a8ded153
Tigran Muradyan <tigran.muradyan140201@gmail.com>
2023-09-19 00:26:23 +0400

    Test Connection endpoint

--- api/controllers/etc.js
+++ api/controllers/etc.js
@@ -1,1 +1,24 @@
+const mysql = require('mysql2');
+const { getEnv } = require('../utils/env');
+
 exports.ping = (_, res) => res.send('pong');
+
+exports.testConnection = async (_, res) => {
+  const connection = mysql.createConnection({
+    host: getEnv('DB_HOST'),
+    user: getEnv('DB_USER'),
+    password: getEnv('DB_PWD'),
+    port: getEnv('DB_PORT'),
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      res.status(500).send({ message: `Connection failed ${err}`, error: err });
+      return;
+    }
+
+    res.status(200).send({ message: 'Connection successful' });
+  });
+
+  connection.end;
+};
--- api/routes/etc.js
+++ api/routes/etc.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const etcController = require('../controllers/etc');
 
 router.get('/ping', etcController.ping);
+router.get('/test-connection', etcController.testConnection);
 
 module.exports = router;
