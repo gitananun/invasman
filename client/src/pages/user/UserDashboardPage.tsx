3a0910aac608d41b9b137d2d602cc7baad697652
Tigran Muradyan <tigran.muradyan140201@gmail.com>
2023-09-19 08:39:32 +0400

    Invasman Project changes

--- client/src/pages/user/UserDashboardPage.tsx
+++ client/src/pages/user/UserDashboardPage.tsx
@@ -3,7 +3,7 @@ import { DashboardLayout } from "../../layouts/DashboardLayout";
 const UserDashboardPage = () => {
   return (
     <DashboardLayout>
-      <h5 className="text-center">Welcome to Sourcemind Inventorization Asset Management Platform!</h5>
+      <h5 className="text-center">Welcome to Inventorization Asset Management Platform!</h5>
     </DashboardLayout>
   );
 };
