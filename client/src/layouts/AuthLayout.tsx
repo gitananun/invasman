76f82388b42a91c77ec29cbf107772be5b190e19
Tigran Muradyan <tigran.muradyan140201@gmail.com>
2023-09-19 11:06:02 +0400

    Demo user details added

--- client/src/layouts/AuthLayout.tsx
+++ client/src/layouts/AuthLayout.tsx
@@ -3,6 +3,11 @@ import "../assets/style/auth.scss";
 import { IChildrenProps } from "../common/interfaces";
 
 export const AuthLayout = (props: IChildrenProps) => {
+  const demoUserDetails = {
+    email: "demo@invasman.com",
+    password: "demo1234",
+  };
+
   return (
     <section id="auth-layout" className="vh-100 d-flex align-items-center justify-content-center overflow-hidden">
       <div className="row vw-100">
@@ -13,7 +18,6 @@ export const AuthLayout = (props: IChildrenProps) => {
               <img src={SourceLogo} alt="Invasman" width={50} />
               <h5 className="my-1">INVASMAN</h5>
               <small className="text-muted">powered by TM</small>
-
               <h4 className="title my-3">Welcome to Inventorization Asset Management</h4>
               <h6 className="text-secondary font-weight-light">Fill your credentials below</h6>
             </div>
@@ -21,6 +25,14 @@ export const AuthLayout = (props: IChildrenProps) => {
             <div className="card-footer mt-4 bg-transparent border-0 d-flex justify-content-center gap-2">
               <small className="text-secondary text-center">
                 Your credentials are stored in a secure database. We will never share your information with anyone.
+              </small>
+            </div>
+            <hr />
+            <div className="card-footer mt-4 bg-transparent border-0 d-flex justify-content-center gap-2">
+              <small className="text-secondary text-center">
+                You can also use the following credentials to login as a demo user.
+                <br />
+                <strong>Email:</strong> {demoUserDetails.email} / <strong>Password:</strong> {demoUserDetails.password}
               </small>
             </div>
           </div>
